fe75559b67bca570be1dbcf5ba17180d6dc38cd7
Shobhit Srivastava <simplyshubh@gmail.com>
2021-10-25 13:28:38 +0530

    Create ChainlinkTest.sol

---
+++ ChainlinkTest.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract ChainlinkTest is ERC721, VRFConsumerBase, Ownable {
+    using SafeMath for uint256;
+    using Strings for string;
+
+    bytes32 internal keyHash;
+    uint256 internal fee;
+    uint256 public randomResult;
+    address public VRFCoordinator;
+    // rinkeby: 0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B
+    address public LinkToken;
+    // rinkeby: 0x01BE23585060835E02B77ef475b0Cc51aA1e0709a
+
+    
+
+
+    mapping(bytes32 => address) requestToSender;
+    mapping(bytes32 => uint256) requestToTokenId;
+
+    /**
+     * Constructor inherits VRFConsumerBase
+     *
+     * Network: Rinkeby
+     * Chainlink VRF Coordinator address: 0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B
+     * LINK token address:                0x01BE23585060835E02B77ef475b0Cc51aA1e0709
+     * Key Hash: 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311
+     */
+    constructor(address _VRFCoordinator, address _LinkToken, bytes32 _keyhash)
+        public
+        VRFConsumerBase(_VRFCoordinator, _LinkToken)
+        ERC721("Chainlink", "Test")
+    {   
+        VRFCoordinator = _VRFCoordinator;
+        LinkToken = _LinkToken;
+ 
+        fee = 0.1 * 10**18; // 0.1 LINK
+    }
+
+    function sendLink(
+        string memory name
+    ) public returns (bytes32) {
+        require(
+            LINK.balanceOf(address(this)) >= fee,
+            "Not enough LINK - fill contract with faucet"
+        );
+        bytes32 requestId = requestRandomness(keyHash, fee);
+        requestToCharacterName[requestId] = name;
+        requestToSender[requestId] = msg.sender;
+        return requestId;
+    }
+}
+
